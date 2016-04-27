0a651ef1238ed91039f5983a12592948a4c85865
Your Name <github3@aaronlux.com>
2016-04-26 17:09:18 -0700

    commit

--- app/src/main/java/w3102931/example/com/consumerestful/Message.java
+++ app/src/main/java/w3102931/example/com/consumerestful/Message.java
@@ -1,5 +1,11 @@
 package w3102931.example.com.consumerestful;
 
+import android.location.Location;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 /**
  * Created by mscs on 4/21/16.
  *     long id;
@@ -10,7 +16,7 @@ package w3102931.example.com.consumerestful;
     public class Message {
 
 
-    public String id;
+    public long id;
     public double latitude;
     public double longitude;
     public long timestamp;
@@ -21,7 +27,7 @@ package w3102931.example.com.consumerestful;
         }
 
 
-        public Message(String id, double latitude, double longitude, long timestamp) {
+        public Message(long id, double latitude, double longitude, long timestamp) {
 
             this.id = id;
             this.latitude = latitude;
@@ -31,7 +37,7 @@ package w3102931.example.com.consumerestful;
 
         }
 
-        public String getId() {
+        public long getId() {
             return this.id;
         }
 
@@ -48,7 +54,7 @@ package w3102931.example.com.consumerestful;
         }
 
 
-        public void setId(String id) {
+        public void setId(long id) {
             this.id = id;
         }
 
@@ -60,12 +66,20 @@ package w3102931.example.com.consumerestful;
             this.longitude = longitude;
         }
 
-         public void setTimestamp(long timestamp) {
+        public void setTimestamp(long timestamp) {
             this.timestamp = timestamp;
         }
 
-}
 
+
+
+
+
+        public static Message message(long id, double latitude, double longitude, long timestamp) {
+            return new Location(id, message.latitude, message.longitude, message.timestamp);
+    }
+
+}
 
 
 
